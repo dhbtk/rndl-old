9c9d7527173db7ac44b57054b30adc25467e8c04
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2017-03-11 15:58:03 -0300

    Action logging

--- app/javascript/store.js
+++ app/javascript/store.js
@@ -1,10 +1,11 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 import initialState from 'reducers/initialState';
 
 export default function configureStore() {
-    return createStore(rootReducer, initialState, applyMiddleware(thunk));
+    return createStore(rootReducer, initialState, applyMiddleware(thunk, createLogger()));
 }
 
 export const store = configureStore();
